434a4d0e33b747478eabf04e513e25b782ffe523
ochoko chukwuebuka <buka4chocksy@gmail.com>
2023-09-19 21:23:06 +0100

    created script that creates a folders and files structure when the file called

---
+++ Week2/automate.sh
@@ -0,0 +1,10 @@
+root_name=www
+mkdir $root_name
+mkdir $root_name/html
+mkdir $root_name/html/img
+mkdir $root_name/html/css
+mkdir $root_name/html/js
+
+touch $root_name/html/img/testing.png
+touch $root_name/html/css/testing.css
+touch $root_name/html/js/testing.js
