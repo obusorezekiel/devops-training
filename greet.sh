94c28b5f4131e05096b5b9ed1b923ceb7e8ad61b
stgreat <salostin@gmail.com>
2023-09-21 12:59:19 -0400

    Added a script to ask for the user's name and greet them

---
+++ greet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script asks for the user's name and greets them.
+
+# Ask the user for their name
+echo "What is your name?"
+read name
+
+# Greet the user with a personalized message
+echo "Hello, $name! Nice to meet you!"
+
