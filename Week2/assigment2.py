94276e2aec46778dc701ba1aab3cbfc954020f58
ochoko chukwuebuka <buka4chocksy@gmail.com>
2023-09-25 20:35:45 +0100

    completed assigment for week2

---
+++ Week2/assigment2.py
@@ -0,0 +1,28 @@
+# import os
+# import shutil
+
+# file_name = input("Enter File name you want to move")
+# folder_path = "../"
+# full_file_path= folder_path + file_name
+# destination_path = "week1/"
+# if os.path.exists(full_file_path)
+#   shutil.copy(full_file_name,destination)
+
+# print("file copied to another folder successfully")
+
+import os
+import shutil
+
+file_name = input("Enter File name you want to move: ")
+folder_path = "./"
+full_file_path = os.path.join(folder_path, file_name)  # Use os.path.join to create a full file path
+print(full_file_path)
+destination_path = "../Week1/"
+
+if os.path.exists(full_file_path):
+    shutil.copy(full_file_path, destination_path)
+    print("File copied to another folder successfully")
+else:
+    print("File not found or destination folder does not exist.")
+
+    
